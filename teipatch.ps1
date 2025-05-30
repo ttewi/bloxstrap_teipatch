eee61c1cc5fcfcdbe0919079c71f40d1d4161b81
tewi <tewi.gloop@gmail.com>
2025-05-30 19:03:24 -0400

    Add files via upload

--- teipatch.ps1
+++ teipatch.ps1
@@ -1,10 +1,16 @@
-$host.ui.rawui.BackgroundColor="Black";cd $PSScriptRoot;$global:args=$args;
+$host.ui.rawui.BackgroundColor="Black";
 
-(ls '.\mods').Name|%{&".\mods\$($_)" "$($global:args[0])"}
+mode concols=96lines=30;cd $psscriptroot;
+$n=[console]::title=$myinvocation.mycommand;
+if(!($m=[threading.mutex]::new(1,'て'+$n)).waitone(0)){return};
+
+(ls '.\mods').Name|%{&".\mods\$($_)" "$($args[0])"}
 & '.\BloxStrap.exe' -player "$($args[0])"
 $($args[0])>.\Logs\uri.txt
 
 echo finished
 
-pause
+#pause
+
+$m.close()
 return
