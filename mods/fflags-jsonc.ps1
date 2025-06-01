0e341fd3bbe33e939583724f286d8f261ff99c16
tewi <tewi.gloop@gmail.com>
2025-05-31 20:01:23 -0400

    Add files via upload

---
+++ mods/fflags-jsonc.ps1
@@ -0,0 +1,63 @@
+#/fn
+    function k($s){
+        $h=@{}
+        $f=@{} #[hashtable]::new()
+        foreach($t in ($s|ConvertFrom-Json).psobject.properties){
+            $a=$t.name;$b=$t.value;
+            $h[$a]=$f[$a]=$b
+        }
+        $n=l($s)
+
+
+        foreach ($t in $h.getenumerator()) {
+            $a=$t.name;$b=$t.value;
+            if ($b.length -eq $null) {$f[$a]=$n[$a]}
+        }
+
+
+        return $f
+    };
+    add-type -assemblyname 'System.Web.Extensions'
+    function l([string]$s) {
+        $parser = New-Object Web.Script.Serialization.JavaScriptSerializer
+        $parser.MaxJsonLength = $s.length
+        Write-Output -NoEnumerate $parser.Deserialize($s, [hashtable])
+
+        #Write-Output -NoEnumerate $parser.DeserializeObject($s)
+        #return $parser.Deserialize($s, [hashtable])
+        # To deserialize to a dictionary, use $parser.DeserializeObject($text) instead
+    }
+    function format-json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
+        $indent = 0;
+        ($json -Split "`n" | % {
+            if ($_ -match '[\}\]]\s*,?\s*$') {$indent--};
+            $line = ('  ' * $indent) + $($_.TrimStart() -replace '":  (["{[])', '": $1' -replace ':  ', ': ');
+            if ($_ -match '[\{\[]\s*$') {$indent++};
+            $line
+        }) -Join "`n"
+    };
+    function json($o){return ($o|convertto-json|format-json)}
+
+
+;## fflags .jsonc :3 srry
+
+$config=(k(gc -path '.\config.json'))
+
+$t=$config.fflagsJsoncLocation
+if(($t -eq $null) -or (!(test-path -path $t))) {
+    $config.fflagsJsoncLocation='.\fflags.jsonc';
+    $io::writealllines('.\config.json',json($config));
+}
+
+$t=$config.fflagsJsoncLocation
+if(!(test-path -path $t)){w(' <red>!! <white>[<yellow>'+$myinvocation.mycommand+'<white>]\n');return}
+
+
+$n=k((gc -Path $t)-replace'(?m)(?<=^([^"]|"[^"]*")*)//.*'-replace'(?ms)/\*.*?\*/')
+$io::writealllines('.\Modifications\ClientSettings\ClientAppSettings.json',json($n))
+
+
+
+w(' <white>[<yellow>'+$myinvocation.mycommand+'<white>]\n')
+
+return
---
+++ mods/multi.ps1
@@ -0,0 +1,75 @@
+
+<#
+iex2(@"
+[console]::title='* $($n-replace'(?<=.*)\..*$','')\multi'
+echo ([console]::title)
+mode concols=120lines=30;
+if(!(`$m=[threading.mutex]::new(1,('ROBLOX_singletonMutex'))).waitone(0)){return};
+
+add-type @`'
+using System;
+using System.Runtime.InteropServices;
+
+public class API {
+
+    public enum SW : int {
+        Hide=0,
+        Normal=1,
+        ShowMinimized=2,
+        Maximize=3,
+        ShowNoActivate=4,
+        Show=5,
+        Minimize=6,
+        ShowMinNoActive=7,
+        ShowNA=8,
+        Restore=9,
+        Showdefault=10,
+        Forceminimize=11
+    }
+
+    [DllImport("user32.dll",
+    EntryPoint="ShowWindow",
+    SetLastError=true,
+    CharSet=CharSet.Unicode,
+    ExactSpelling=true,
+    CallingConvention=CallingConvention.StdCall)]
+
+    public static extern int ShowWindow(IntPtr hwnd, SW nCmdShow);
+}
+`'@
+
+
+`$h=([system.diagnostics.process]::getcurrentprocess().mainwindowhandle)
+
+
+#pause
+
+
+
+#[api]::ShowWindow(`$h,"show")
+#[api]::ShowWindow(`$h,"restore")
+mode concols=50lines=15;
+
+
+sleep 
+[api]::ShowWindow(`$h,"hide")
+
+
+
+
+
+
+pause
+"@)
+#>
+
+start powershell -windowstyle hidden {
+    mode concols=50lines=15;
+    if(![threading.mutex]::new(1,([console]::title='ROBLOX_singletonMutex')).waitone(0)){return};
+    pause
+};
+
+
+w(' <white>[<cyan>'+$myinvocation.mycommand+'<white>]\n')
+
+return
