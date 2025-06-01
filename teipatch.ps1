e4cd47c46b7cc8c93553dc66d320582e0da8e620
tewi <tewi.gloop@gmail.com>
2025-05-31 20:49:25 -0400

    Add files via upload

--- teipatch.ps1
+++ teipatch.ps1
@@ -46,7 +46,7 @@ $io=[io.file]
 $utf8=[System.Text.Encoding]::UTF8
 
 (ls '.\mods').Name|%{&".\mods\$($_)" "$($args[0])"}
-#& '.\BloxStrap.exe' -player "$($args[0])"
+& '.\BloxStrap.exe' -player "$($args[0])"
 $($args[0])>.\Logs\uri.txt
 
 
